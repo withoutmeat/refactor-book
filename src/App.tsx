c5d64af8d44f32c356d4789eab24f055ace4d40c
withoutmeat <withoutmeat@qq.com>
2021-12-04 15:33:21 +0800

    refactor: 内联变量(123)

--- src/App.tsx
+++ src/App.tsx
@@ -73,16 +73,15 @@ function statement(invoice: Invoice, plays: Plays) {
       minimumFractionDigits: 2
     }).format;
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(play, perf);
+    let thisAmount = amountFor(playFor(perf), perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\r\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\r\n`;
     totalAmount += thisAmount;
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;

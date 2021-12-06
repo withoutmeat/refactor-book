06cd20eed6136aede9e41f4dfb87dd85a9b53757
withoutmeat <withoutmeat@qq.com>
2021-12-06 23:10:30 +0800

    refactor: 以多态取代条件表达式(272)

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -1,4 +1,10 @@
-export type Play = { name: string, type: string }
+/**
+ * 1. 将函数拆分成一组嵌套的函数
+ * 2. 分离计算逻辑和输出格式化逻辑
+ * 3. 为计算机引入多态特性来处理计算逻辑
+ */
+
+export type Play = { name: string, type: 'comedy' | 'tragedy' }
 /**
  * 所有剧目
  */
@@ -36,6 +42,51 @@ export type StatementData =
 	& { performances: EnrichmentPerformance[], totalAmount: number, totalVolumeCredits: number }
 
 
+export default function createStatementData(invoice: Invoice, plays: Plays) {
+	const result = {} as StatementData
+	result.customer = invoice.customer
+	result.performances = invoice.performances.map(enrichPerformance)
+	result.totalAmount = totalAmount(result)
+	result.totalVolumeCredits = totalVolumeCredits((result))
+	return result
+
+	/**
+	 * 填充
+	 * @param performance
+	 */
+	function enrichPerformance(performance: Performance) {
+		const calculator = createPerformanceCalculator(performance, playFor((performance)))
+		const result = { ...performance } as EnrichmentPerformance
+		result.play = calculator.play
+		result.amount = calculator.amount
+		result.volumeCredits = calculator.volumeCredits
+		return result
+	}
+
+	function playFor(performance: Performance) {
+		return plays[performance.playID]
+	}
+
+	function totalAmount(data: StatementData) {
+		return data.performances.reduce((total, p) => total + p.amount, 0)
+	}
+
+	function totalVolumeCredits(data: StatementData) {
+		return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
+	}
+}
+
+function createPerformanceCalculator(performance: Performance, play: Play) {
+	switch (play.type) {
+		case 'comedy':
+			return new ComedyCalculator(performance, play)
+		case 'tragedy':
+			return new TragedyCalculator(performance, play)
+		default:
+			throw new Error(`unknown type ${play.type}`)
+	}
+}
+
 class PerformanceCalculator {
 	/**
 	 * 演出计算器
@@ -48,92 +99,47 @@ class PerformanceCalculator {
 	/**
 	 * 演出价格
 	 */
-	get amount() {
-		let result: number = 0
-		switch (this.play.type) {
-			case 'tragedy':
-				result = 40000
-				if (this.performance.audience > 30) {
-					result += 1000 * (this.performance.audience - 30)
-				}
-				break
-			case 'comedy':
-				result = 30000
-				if (this.performance.audience > 20) {
-					result += 10000 + 500 * (this.performance.audience - 20)
-				}
-				result += 300 * this.performance.audience
-				break
-			default:
-				throw new Error(`unknown type: ${this.play.type}`)
-		}
-		return result
+	get amount(): number {
+		throw new Error('subclass responsibility')
 	}
 
 	/**
 	 * 观众量积分
 	 */
 	get volumeCredits() {
-		let result = 0
-		// add volume credits
-		result += Math.max(this.performance.audience - 30, 0)
-		// add extra credit for every ten comedy attendees
-		if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5)
+		return Math.max(this.performance.audience - 30, 0)
+	}
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+	/**
+	 * 演出价格
+	 */
+	get amount() {
+		let result = 30000
+		if (this.performance.audience > 20) {
+			result += 10000 + 500 * (this.performance.audience - 20)
+		}
+		result += 300 * this.performance.audience
+		return result
+	}
+
+	get volumeCredits() {
+		// @ts-ignore
+		return super.volumeCredits + Math.floor(this.performance.audience / 5)
+	}
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+	/**
+	 * 演出价格
+	 */
+	get amount() {
+		let result: number = 40000
+		if (this.performance.audience > 30) {
+			result += 1000 * (this.performance.audience - 30)
+		}
 		return result
 	}
 }
 
-function createPerformanceCalculator(performance: Performance, play: Play) {
-	return new PerformanceCalculator(performance, play)
-}
-
-export default function createStatementData(invoice: Invoice, plays: Plays) {
-	const result = {} as StatementData
-	result.customer = invoice.customer
-	result.performances = invoice.performances.map(enrichPerformance)
-	result.totalAmount = totalAmount(result)
-	result.totalVolumeCredits = totalVolumeCredits((result))
-
-	return result
-
-	/**
-	 * 填充
-	 * @param performance
-	 */
-	function enrichPerformance(performance: Performance) {
-		const calculator = createPerformanceCalculator(performance, playFor((performance)))
-		const result = { ...performance } as EnrichmentPerformance
-		result.play = calculator.play
-		result.amount = amountFor(performance)
-		result.volumeCredits = volumeCreditsFor((performance))
-		return result
-	}
-
-	function playFor(performance: Performance) {
-		return plays[performance.playID]
-	}
-
-	/**
-	 * 计算一场演出的费用
-	 * @param performance 包含剧目id和观众数量
-	 */
-	function amountFor(performance: Performance) {
-		return new PerformanceCalculator(performance, playFor(performance)).amount
-	}
-
-	/**
-	 *  计算观众量积分
-	 * @param performance 包含剧目id和观众数量
-	 */
-	function volumeCreditsFor(performance: Performance) {
-		return new PerformanceCalculator(performance, playFor(performance)).volumeCredits
-	}
-
-	function totalAmount(data: StatementData) {
-		return data.performances.reduce((total, p) => total + p.amount, 0)
-	}
-
-	function totalVolumeCredits(data: StatementData) {
-		return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
-	}
-}

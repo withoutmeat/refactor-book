ae35eb74b58aacc1826dc9a2887bbad0525dede6
withoutmeat <withoutmeat@qq.com>
2021-12-06 22:39:30 +0800

    refactor: 搬移函数(198)amountFor,volumeCreditsFor

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -36,6 +36,53 @@ export type StatementData =
 	& { performances: EnrichmentPerformance[], totalAmount: number, totalVolumeCredits: number }
 
 
+class PerformanceCalculator {
+	/**
+	 * 演出计算器
+	 * @param performance 演出
+	 * @param play 剧目
+	 */
+	constructor(public performance: Performance, public play: Play) {
+	}
+
+	/**
+	 * 演出价格
+	 */
+	get amount() {
+		let result: number = 0
+		switch (this.play.type) {
+			case 'tragedy':
+				result = 40000
+				if (this.performance.audience > 30) {
+					result += 1000 * (this.performance.audience - 30)
+				}
+				break
+			case 'comedy':
+				result = 30000
+				if (this.performance.audience > 20) {
+					result += 10000 + 500 * (this.performance.audience - 20)
+				}
+				result += 300 * this.performance.audience
+				break
+			default:
+				throw new Error(`unknown type: ${this.play.type}`)
+		}
+		return result
+	}
+
+	/**
+	 * 观众量积分
+	 */
+	get volumeCredits() {
+		let result = 0
+		// add volume credits
+		result += Math.max(this.performance.audience - 30, 0)
+		// add extra credit for every ten comedy attendees
+		if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5)
+		return result
+	}
+}
+
 export default function createStatementData(invoice: Invoice, plays: Plays) {
 	const result = {} as StatementData
 	result.customer = invoice.customer
@@ -50,8 +97,9 @@ export default function createStatementData(invoice: Invoice, plays: Plays) {
 	 * @param performance
 	 */
 	function enrichPerformance(performance: Performance) {
+		const calculator = new PerformanceCalculator(performance, playFor(performance))
 		const result = { ...performance } as EnrichmentPerformance
-		result.play = playFor(performance)
+		result.play = calculator.play
 		result.amount = amountFor(performance)
 		result.volumeCredits = volumeCreditsFor((performance))
 		return result
@@ -66,38 +114,15 @@ export default function createStatementData(invoice: Invoice, plays: Plays) {
 	 * @param performance 包含剧目id和观众数量
 	 */
 	function amountFor(performance: Performance) {
-		let result: number = 0
-		switch (playFor(performance).type) {
-			case 'tragedy':
-				result = 40000
-				if (performance.audience > 30) {
-					result += 1000 * (performance.audience - 30)
-				}
-				break
-			case 'comedy':
-				result = 30000
-				if (performance.audience > 20) {
-					result += 10000 + 500 * (performance.audience - 20)
-				}
-				result += 300 * performance.audience
-				break
-			default:
-				throw new Error(`unknown type: ${playFor(performance).type}`)
-		}
-		return result
+		return new PerformanceCalculator(performance, playFor(performance)).amount
 	}
 
 	/**
 	 *  计算观众量积分
-	 * @param perf 包含剧目id和观众数量
+	 * @param performance 包含剧目id和观众数量
 	 */
-	function volumeCreditsFor(perf: Performance) {
-		let result = 0
-		// add volume credits
-		result += Math.max(perf.audience - 30, 0)
-		// add extra credit for every ten comedy attendees
-		if ('comedy' === playFor(perf).type) result += Math.floor(perf.audience / 5)
-		return result
+	function volumeCreditsFor(performance: Performance) {
+		return new PerformanceCalculator(performance, playFor(performance)).volumeCredits
 	}
 
 	function totalAmount(data: StatementData) {
